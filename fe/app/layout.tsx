df4090365beef985ade0bac973656facb27aac5d
phucnh204 <nguyenhuuphuc2004.ct@gmail.com>
2025-12-08 16:59:59 +0700

    Update layout.tsx

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata = {
     images: ["https://camdocantho.net/logo-removebg-small.png"],
   },
 
-  
+
   twitter: {
     card: "summary_large_image",
     title: "Camdocantho.net - Cầm đồ Nhựt Tân",
@@ -44,4 +44,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </body>
     </html>
   );
+  
 }
